21d44b284fb65033a535c122232c2ffdc2445d65
alyona706 <almokina@gmail.com>
2025-03-12 18:48:26 +0200

    Add 11.3

--- 11.3/script.js
+++ 11.3/script.js
@@ -1,3 +1,20 @@
 function showRandomImg() {
-    let randomNumber = Math.floor(Math.random() * 7) + 1;
+    let button = document.querySelector('button');
+
+    button.addEventListener('click', () => {
+        let randomNumber = Math.floor(Math.random() * 7) + 1;
+
+        const imageWrap = document.querySelector('#imgWrapper');
+        let oldImage = imageWrap.querySelector('img');
+
+        if(!oldImage) {
+            image = document.createElement('img');
+            image.setAttribute('src', `images/${randomNumber}.jpg`);
+            imageWrap.appendChild(image);
+        } else {
+            oldImage.setAttribute('src', `images/${randomNumber}.jpg`);
+        }
+    });
 }
+
+ showRandomImg();
